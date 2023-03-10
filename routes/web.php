6a5673f1c762c16c47c14dbc63ea315c27f39bf3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-03-10 11:15:28 -0300

    Facilidades

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::get('/series', 'SeriesController@index')
-    ->name('listar_series');
-Route::get('/series/criar', 'SeriesController@create')
-    ->name('form_criar_serie');
-
-Route::post('/series/criar', 'SeriesController@store');
-Route::delete('/series/{id}', 'SeriesController@destroy');
