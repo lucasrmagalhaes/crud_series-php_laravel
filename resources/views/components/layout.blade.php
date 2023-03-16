e318327ee4416ae5c67aa7c647ab2afefafe9007
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-03-16 05:34:50 -0300

    Estilizando as views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,14 +6,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link ref="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
 
         <title>{{ $title }} - Controle de Séries</title>
     </head>
 
     <body>
-        <h1>{{ $title }}</h1>
+        <div class="container">
+            <h1>{{ $title }}</h1>
 
-        {{ $slot }}
+            {{ $slot }}
+        </div>
     </body>
 </html>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,14 @@
 <x-layout title="Séries">
-    <a href="/series/criar">Adicionar</a>
+    <a
+        href="/series/criar"
+        class="btn btn-dark mb-2"
+    >
+        Adicionar
+    </a>
 
-    <ul>
+    <ul class="list-group">
         @foreach ($series as $serie)
-            <li>{{ $serie }}</li>
+            <li class="list-group-item">{{ $serie }}</li>
         @endforeach
     </ul>
 </x-layout>
